ae2d8839b0c6448f39fbd1bc044b52595db20066
nereLuiza <151404346+nereLuiza@users.noreply.github.com>
2024-11-21 19:15:34 +0000

    add DB

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-import express from "express";      // Requisição do pacote do express
+import dotenv from "dotenv";
+import express from "express";
+import { selectUsuarios } from "./bd.js";
 
-const app = express();              // Instancia o Express
-const port = 3000;                  // Define a porta
+dotenv.config();
 
-app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
-  console.log("Rota GET/ solicitada");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  console.log("Rota / solicitada");
+  // Cria a rota da raiz do projeto
   res.json({
-    nome: "Luíza C. Nere",      // Substitua pelo seu nome
+    nome: "Luíza Nere",
   });
 });
 
-app.listen(port, () => {            // Um socket para "escutar" as requisições
-  console.log(`Serviço escutando na porta:  ${port}`);
+app.get("/usuarios", async (req, res) => {
+  try {
+    const usuarios = await selectUsuarios();
+    res.json(usuarios);
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+
+  console.log("Rota GET/usuarios solicitada");
 });
 
+app.listen(port, () => {
+  // Um socket para "escutar" as requisições
+  console.log(`Serviço escutando na porta:  ${port}`);
+});
