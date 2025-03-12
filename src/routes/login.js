2b13e2d814082046eddc2e5b40d323e31c281c63
nereLuiza <151404346+nereLuiza@users.noreply.github.com>
2025-03-12 16:58:35 +0000

    add auth

--- src/routes/login.js
+++ src/routes/login.js
@@ -19,4 +19,13 @@ router.post("/login", async (req, res) => {
   }
 });
 
+router.get("/auth", verificarAutenticacao, async (req, res) => {
+  console.log("Rota GET /auth solicitada");
+  try {
+    res.status(200).json({ user: `${req.userId}` });
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
+
 export default router;
